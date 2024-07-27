8ba25fe594bb51325eb320a410f77865cdde78e3
Moses Etyang <etyangm98@gmail.com>
2024-07-27 14:40:28 +0300

    Update train_models.py

--- train_models.py
+++ train_models.py
@@ -4,11 +4,48 @@ from sklearn.neighbors import KNeighborsRegressor
 from sklearn.feature_extraction.text import TfidfVectorizer
 import pickle
 import os
+import requests
+
+# Google Drive direct download links
+file_links = {
+    'nyc': 'https://drive.google.com/uc?export=download&id=1U5LY2d6MPwVVjhECl5m8MpFpo-bDPLxj',
+    'berlin': 'https://drive.google.com/uc?export=download&id=13MaITpBsID3lHpPadgDIMFJNNDs4nA5l',
+    'amsterdam': 'https://drive.google.com/uc?export=download&id=1MAB_koP-CPDkhjVs3DUbeU_jLuD3HCMD',
+    'sydney': 'https://drive.google.com/uc?export=download&id=1l0MY9e3FoV-qNkD9arAdjo8qIXvy4H0T',
+    'rome': 'https://drive.google.com/uc?export=download&id=1ua6XGqqGCNq6yHBtFYxo6_DuH9sOfURD',
+    'tokyo': 'https://drive.google.com/uc?export=download&id=1CI_JxuNs2ZY0rCHiw_YoE8SJg8lLME1w',
+    'barcelona': 'https://drive.google.com/uc?export=download&id=1vGDtFTscAXPHdtFHcbOBo47NW_qzEDSd',
+    'brussels': 'https://drive.google.com/uc?export=download&id=19EaSibkDr7VGdUd5Afo_JF1rMe_8MoEy'
+}
+
+def download_file_from_google_drive(url, destination):
+    response = requests.get(url, stream=True)
+    with open(destination, 'wb') as f:
+        for chunk in response.iter_content(chunk_size=32768):
+            if chunk:
+                f.write(chunk)
+
+def load_data(city):
+    file_path = f'./datasets/{city}_airbnb_listings.csv'
+    if not os.path.exists(file_path):
+        os.makedirs(os.path.dirname(file_path), exist_ok=True)
+        file_link = file_links.get(city)
+        if not file_link:
+            print(f"No file link found for city: {city}")
+            return None
+        download_file_from_google_drive(file_link, file_path)
+    try:
+        return pd.read_csv(file_path)
+    except Exception as e:
+        print(f"Error loading data for {city}: {e}")
+        return None
 
 def train_and_save_models(city):
     # Load dataset
-    file_path = f'./datasets/{city}/{city}_airbnb_listings.csv'
-    data = pd.read_csv(file_path, low_memory=False)
+    data = load_data(city)
+    if data is None:
+        print(f"Data for city {city} could not be loaded.")
+        return
 
     # Handle missing values
     data.fillna({
@@ -72,4 +109,3 @@ if __name__ == "__main__":
         print(f'Training models for {city}...')
         train_and_save_models(city)
         print(f'Models for {city} trained and saved.')
-        
